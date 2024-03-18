965fa8324d0ed6cd7fe30ce2beaa0b488eeb41f3
521py <95997423+521py@users.noreply.github.com>
2024-03-18 21:16:18 +0700

    feat: added header

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,9 +1,5 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
-import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
-import { Field } from '@/components/ui/field/field.component'
-import { UserItem } from '@/components/ui/user-item/user-item.component'
-// import { Button } from '@/components/ui/button/button.component'
 import styles from './home.module.scss'
 import template from './home.template.html'
 
@@ -13,28 +9,7 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(
-			template,
-			[
-				new Field({
-					name: 'test',
-					placeholder: 'enter email',
-					variant: 'green'
-				}),
-				new UserItem(
-					{
-						avatarPath:
-							'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Tennis_ball.svg/240px-Tennis_ball.svg.png',
-						name: 'No Name'
-					},
-					false,
-					() => alert('Hey')
-				)
-			],
-			styles
-		)
-
-		$R(element).find('h1').css('color', 'blue')
+		const element = renderService.htmlToElement(template, [], styles)
 
 		return element
 	}
